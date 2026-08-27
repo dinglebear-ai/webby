a524b7ec08bb63172cf1554f883f05b85e35e9e0
Jacob Magar <jmagar@gmail.com>
2026-08-27 09:43:49 -0400

    fix: clear rejected browser Origin identity

--- lib/webby_web/browser_origin.ex
+++ lib/webby_web/browser_origin.ex
@@ -4,6 +4,8 @@ defmodule WebbyWeb.BrowserOrigin do
   @process_key {__MODULE__, :extension_id}
 
   def allowed?(%URI{scheme: "chrome-extension", host: extension_id}) do
+    clear_extension_id()
+
     if is_binary(extension_id) and String.match?(extension_id, ~r/\A[a-p]{32}\z/) do
       # Phoenix's origin-check callback receives the parsed Origin but not the
       # connection. The socket connect callback runs synchronously in this same
@@ -16,7 +18,12 @@ defmodule WebbyWeb.BrowserOrigin do
     end
   end
 
-  def allowed?(_uri), do: false
+  def allowed?(_uri) do
+    clear_extension_id()
+    false
+  end
 
   def take_extension_id, do: Process.delete(@process_key)
+
+  defp clear_extension_id, do: Process.delete(@process_key)
 end
